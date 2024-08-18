20b85b82264169365c9dbdcea2475d72d64ada0b
Earston-G-Dev <markus.williams95.dev@gmail.com>
2024-08-18 16:52:02 -0700

    Updated learning

---
+++ Session 2/Code2.js
@@ -0,0 +1,5 @@
+// Starting by defining a age.
+const age = 23;
+// 1.We start the conditional by stating what the age condition will be.
+//2.After the condition we use a question mark
+age >= 18 ? console.log('I like to drink wine 😒') :  console.log('I like to drink water 😊') 
---
+++ Session 2/Session2ReadME.md
@@ -0,0 +1,1 @@
+This is where im going to practice conditional ternery operations.
